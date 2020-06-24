6b5a15e3edf8987ad04418bd47fdc1891be131c8
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-06-24 15:16:37 +0100

    AddingTheLoginLogictoTheStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
 
 Vue.use(Vuex)
 
@@ -10,7 +12,10 @@ export default new Vuex.Store({
       {name:"Shiny Star",price:40},
       {name:"Green Shels",price:60},
       {name:"Red Shels",price:80},
-    ]
+    ],
+    status: '',
+    token: localStorage.getItem('token') || '',
+    user : {}
   },
   getters:{
   saleProducts:state=>{
@@ -37,12 +42,28 @@ export default new Vuex.Store({
         context.commit('reducePrice',payload)
       },2000)
     },
-    // login:context=>{
+    login({commit}, user){
+      return new Promise((resolve, reject) => {
+        commit('auth_request')
+        axios({url: 'http://localhost:8000/api/login', data: user, method: 'POST' })
+        .then(resp => {
+          const token = resp.data.token
+          const user = resp.data.user
+          localStorage.setItem('token', token)
+          axios.defaults.headers.common['Authorization'] = token
+          commit('auth_success', token, user)
+          resolve(resp)
+        })
+        .catch(err => {
+          commit('auth_error')
+          localStorage.removeItem('token')
+          reject(err)
+        })
+      })
+  },
+  
 
-    // },
-    // register:context=>{
-
-    // }
+ 
   },
   modules: {
   }
